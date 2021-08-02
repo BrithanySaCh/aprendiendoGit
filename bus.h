d2d133035b73d2a8d7502dbe324a5a1a53017369
Brithany <salazarbrithany418@gmail.com>
2021-08-01 21:40:35 -0600

    Inicio

---
+++ bus.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "chofer.h"
+#include "ruta.h"
+
+class Bus {
+private:
+	string placa;
+	Chofer* choferBus;
+	Ruta* rutaBus;
+public:
+	Bus(string plac = "", Chofer* driver = NULL, Ruta* rute = NULL);
+	virtual ~Bus();
+	string getPlaca();
+	Chofer* getChofer();
+	Ruta* getRuta();
+	void setPlaca(string plac);
+	void setChofer(Chofer* driver);
+	void setRuta(Ruta* rute);
+	string toString();
+};
